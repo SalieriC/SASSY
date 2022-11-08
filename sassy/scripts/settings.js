9724aa0015182f27a3f4917b977cee17c43d852f
SalieriC <salieric@t-online.de>
2022-11-09 00:12:23 +0100

    setting templates

--- sassy/scripts/settings.js
+++ sassy/scripts/settings.js
@@ -1,3 +1,26 @@
 export function register_settings() {
     //TBD
+
+    /* Colour picker template
+    new window.Ardittristan.ColorSetting('sassy', 'stuff', {
+        name: game.i18n.localize('SASSY.stuffName'),
+        hint: game.i18n.localize('SASSY.stuffHint'),
+        label: game.i18n.localize('SASSY.stuffButton'),
+        restricted: true,
+        defaultColor: '#ffffff',
+        config: true,
+        scope: 'world',
+    })
+    */
+    /* Image picker template
+    game.settings.register('sassy', 'stuff', {
+        name: game.i18n.localize("SASSY.stuffName"),
+        hint: game.i18n.localize("SASSY.stuffHint"),
+        type: window.Azzu.SettingsTypes.FilePickerImage,
+        default: '',
+        restricted: true,
+        scope: 'world',
+        config: true,
+    })
+    */
 }
