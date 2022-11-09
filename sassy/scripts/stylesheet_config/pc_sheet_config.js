13e72771c61e2326b6c24da14312cfa31c7f6aa9
SalieriC <salieric@t-online.de>
2022-11-09 01:21:14 +0100

    removed console.log

--- sassy/scripts/stylesheet_config/pc_sheet_config.js
+++ sassy/scripts/stylesheet_config/pc_sheet_config.js
@@ -2,7 +2,6 @@ export async function pc_sheet_config(app, html, _) {
     //Disable Branding:
     const disableBranding = game.settings.get('sassy', 'disableBranding')
     if (disableBranding) {
-        console.log(html)
         html.find('figure.gridcell.branding').css('display', 'none')
         html.find('.charname-container').css('background', 'transparent')
         html.find('.charname-container').css('min-width', '530px')
